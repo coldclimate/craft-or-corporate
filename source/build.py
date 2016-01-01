b19b4eced0c4602be95b57a31fe03cd4435145a3
coldclimate <oliver@coldclimate.co.uk>
2016-01-01 20:16:22 +0000

    A dirty build mechism
    data.json unordered on purpose to make sure my sort works

--- source/build.py
+++ source/build.py
@@ -5,6 +5,7 @@ text_file = codecs.open("output.html", "w", "utf-8")
 
 with open('data.json') as data_file:    
     data = json.load(data_file)
+    data.sort(key=lambda tup: tup["name"])
     for item in data:
     	try:
     		text_file.write("<li class=\"%s\"><a href=\"%s\">%s</a> <a href=\"%s\">citation</a></li>" % (item["status"], item["link"], item["name"], item["citation"]))
