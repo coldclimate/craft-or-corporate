f20b0f43998790234c616c6cf2acbd852546f597
box <box>
2017-07-13 19:41:15 +0100

    a dirty hack to get my build working again before merging other things

--- source/build.py
+++ source/build.py
@@ -7,6 +7,8 @@ with open('data.json') as data_file:
     data = json.load(data_file)
     data.sort(key=lambda tup: tup["name"])
     for item in data:
+    	if 'link' not in item:
+    		item["link"] = item["citation"]
     	try:
     		text_file.write("<li class=\"%s\"><a href=\"%s\">%s</a> <a href=\"%s\">citation</a></li>" % (item["status"], item["link"], item["name"], item["citation"]))
     	except KeyError:
